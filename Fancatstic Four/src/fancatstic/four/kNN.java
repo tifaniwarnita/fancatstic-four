36b29eef3b93b76978fd81e9d79905dc250feca0
tifaniwarnita <tiffayumuyuka@gmail.com>
2015-11-20 14:15:26 +0700

    Refactor kNN

--- Fancatstic Four/src/fancatstic/four/kNN.java
+++ Fancatstic Four/src/fancatstic/four/kNN.java
@@ -13,22 +13,25 @@ import java.util.*;
  */
 public class kNN {
     private int k;
+    private DataSet data;
     private PriorityQueue<Neighbor> neighbors;
     private List<DataClass> classes = new ArrayList<>();
     
     public kNN() {
-        
+        // Empty constructor
     }
     
-    public kNN(DataSet data, List<String> identify, int _k) {
+    public kNN(DataSet _data, List<String> identify, int _k) {
         k = _k;
+        data = _data;
         neighbors = new PriorityQueue(k, new Comparator<Neighbor>() {
             @Override
             public int compare(Neighbor neighbor1, Neighbor neighbor2) {
-                return Integer.compare(neighbor1.getDistance(),neighbor2.getDistance());
+                return Integer.compare(neighbor2.distance, neighbor1.distance);
             }
-    }   );
+        });
         
+        // Menghitung jarak dari setiap atribut
         for (int i=0; i<data.getDataset().size(); i++) {
             int dis = 0;
             for (int j=0; j<data.getDataset().get(i).size()-1; j++) {
@@ -37,82 +40,100 @@ public class kNN {
                 } else {
                     dis++;
                 }
-                //System.out.print(data.getData().get(i).get(j) + " " + identify.get(j));
+                System.out.print(data.getDataset().get(i).get(j) + " ");
             }
-            Neighbor n = new Neighbor(i,data.getDataset().get(i),dis);
+            System.out.println("distance: " + dis);
+            Neighbor n = new Neighbor(i,dis);
             neighbors.add(n);
+            if (neighbors.size() > k)
+                neighbors.poll();
         }
-        /*for (int i=0; i<data.getDataset().size(); i++) {
-            System.out.println(neighbors.get(i).getIndex() + ", " + neighbors.get(i).getDistance());
-        }
-        */
+
     }
     
     public void printQueue(){
-        PriorityQueue<Neighbor> copy = new PriorityQueue<Neighbor>(neighbors);
-            
-        for(int i=0; i<k; i++){
-                System.out.println(copy.peek().getIndex() + " - " + copy.peek().getDistance());
-                copy.remove();
+        Queue<Neighbor> copy = new PriorityQueue<Neighbor>(neighbors);
+        
+        Iterator<Neighbor> itr = copy.iterator();
+        System.out.println("SIZE: " + copy.size());
+        while(itr.hasNext()){
+            Neighbor n = copy.poll();
+            System.out.println(n.index + " - " + n.index);
         }
     }
     
-    public String solve(){
-        // Mencari jumlah masing-masing kelas pada array Neighbor
+    public String solve() {
         PriorityQueue<Neighbor> copy = new PriorityQueue<Neighbor>(neighbors);
-        for(int i=0; i<k; i++){
-            List<String> data = new ArrayList<>(copy.peek().getData());
-            copy.remove();
-            // Jika list masih kosong, elemen data akan dimasukkan
-            if (classes.isEmpty()){
-                DataClass newData = new DataClass(data.get(data.size()-1));
-                classes.add(newData);
-            }
-            else {
-                boolean found = false;
-                int j = 0;
-                while ((j < classes.size()) && !found){
-                    // Jika kelas yang sama sudah ada, maka counter hanya akan bertambah
-                    if (data.get(data.size()-1).equals(classes.get(j).getClassData())){
-                        classes.get(j).addCounter();
-                        found = true;
-                    }
+        
+        // Mencari jumlah masing-masing kelas pada priority queue Neighbor
+        Iterator<Neighbor> itr = copy.iterator();
+        while(itr.hasNext()){
+            Neighbor n = itr.next();
+            
+            boolean found = false;
+            int j = 0;
+            while ((j < classes.size()) && !found){
+                // Jika kelas yang sama sudah ada, maka counter hanya akan bertambah
+                if (data.getClass(n.index).equals(classes.get(j).classData)){
+                    classes.get(j).addCounter();
+                    found = true;
+                } else {
                     j++;
                 }
-                
-                // Jika kelas belum ada pada list, maka kelas akan ditambahkan
-                if (!found && j == classes.size()){
-                    DataClass newData = new DataClass(data.get(data.size()-1));
-                    classes.add(newData);
-                }
+            }
+            // Jika kelas belum ada pada list, maka kelas akan ditambahkan
+            if (!found){
+                DataClass newData = new DataClass(data.getClass(n.index));
+                classes.add(newData);
             }
         }
-        
-        /* Mengeprint jumlah data dari setiap kelas
-        for(int i=0; i<classes.size(); i++){
-            classes.get(i).printData();
-        }
-        */
         classes.sort(new Comparator<DataClass>(){
-            @Override
-            public int compare(DataClass d1, DataClass d2) {
-                return d1.getCount() < d2.getCount() ? 1 : -1;
-            }
+                @Override
+                public int compare(DataClass d1, DataClass d2) {
+                    return (d1.count < d2.count ? 1 : -1);
+                }
         });
         
-        return classes.get(0).getClassData();
+        return classes.get(0).classData;
+    }
+    
+    //Nested class DataClass
+    public class DataClass {
+        public String classData;
+        public int count;
+
+        public DataClass(){
+            this.classData = "";
+            this.count = 1;
+        }
+
+        public DataClass(String _classData){
+            this.classData = new String(_classData);
+            this.count = 1;
+        }
         
+        public void addCounter() {
+            this.count++;
+        }
+
+        public void printData(){
+            System.out.println(classData + " - " + count);
+        }
     }
-    /*
-    public String solve(){
-        countClasses();
-        classes.sort(new Comparator<DataClass>(){
-            @Override
-            public int compare(DataClass d1, DataClass d2) {
-                return d1.getCount() < d2.getCount() ? 1 : -1;
-            }
-    });
-        return classes.get(0).getClassData();
+    
+    //Nested class Neighbor
+    public class Neighbor {
+        public int index;
+        public int distance;
+
+        public Neighbor(){
+            this.index = 0;
+            this.distance = 0;
+        }
+
+        public Neighbor(int _index, int _distance){
+            this.index = _index;
+            this.distance = _distance;
+        }
     }
-    */
 }
